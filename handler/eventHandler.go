99c35a887f66f01650279134866dbeac47969b38
zetto <ritz143@naver.com>
2025-02-14 01:03:54 +0900

    Update eventHandler.go

--- handler/eventHandler.go
+++ handler/eventHandler.go
@@ -1,13 +1,13 @@
-// handler/eventHandler.go
 package handler
 
 import (
-	"github.com/bwmarrin/discordgo"
-	"github.com/zetto21/discordgo/events/client"
+    "github.com/bwmarrin/discordgo"
+    "github.com/zetto21/discordgo/events/client"
+    "github.com/zetto21/discordgo/events/guilds"
 )
 
 func RegisterEventHandlers(dg *discordgo.Session) {
-	dg.AddHandler(client.Ready)
-	// dg.AddHandler(guilds.InteractionCreate)
-	// dg.AddHandler(guilds.MessageCreate)
+    dg.AddHandler(client.Ready)
+    dg.AddHandler(guilds.InteractionCreate)
+    dg.AddHandler(guilds.MessageCreate)
 }
