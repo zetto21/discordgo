90b7c80856cf302dde0bffcf0fab72877746e4d9
zetto <ritz143@naver.com>
2025-02-14 01:09:28 +0900

    Update setting.go

--- utils/setting.go
+++ utils/setting.go
@@ -4,6 +4,12 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
+type Command struct {
+    Name    string
+    Aliases []string
+    Execute func(*discordgo.Session, *discordgo.MessageCreate, []string)
+}
+
 // getGuildName retrieves the name of the guild by its ID
 func GetGuildName(s *discordgo.Session, guildID string) string {
 	guild, err := s.State.Guild(guildID)
