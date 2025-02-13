52cb75cd798d1e58eab2f0cebc3811eb918b54c6
zetto <ritz143@naver.com>
2025-02-14 01:09:14 +0900

    Update pnig.go

--- commands/message/pnig.go
+++ commands/message/pnig.go
@@ -3,14 +3,22 @@ package message
 import (
 	"fmt"
 	"github.com/bwmarrin/discordgo"
+	"github.com/zetto21/discordgo/utils"
 )
 
+// 봇 정보 명령어 정의
+var Pingcommand = utils.Command{
+    Name:    "핑",
+    Aliases: []string{"vld", "ping","ㅍ"},
+    Execute: PingMessageCommand,
+}
+
 func PingMessageCommand(s *discordgo.Session, m *discordgo.MessageCreate, args []string) {
-	latency := s.HeartbeatLatency().Milliseconds()
-	response := fmt.Sprintf("퐁! %dms", latency)
-	_, _ =  s.ChannelMessageSendReply(m.ChannelID, response, &discordgo.MessageReference{
-		MessageID: m.ID,
-		ChannelID: m.ChannelID,
-		GuildID:   m.GuildID,
-	})
+    latency := s.HeartbeatLatency().Milliseconds()
+    response := fmt.Sprintf("퐁! %dms", latency)
+    _, _ = s.ChannelMessageSendReply(m.ChannelID, response, &discordgo.MessageReference{
+        MessageID: m.ID,
+        ChannelID: m.ChannelID,
+        GuildID:   m.GuildID,
+    })
 }
